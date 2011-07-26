c142849b5723597e674297deb64dffc36df1399d
Kohei Yasumura <yasumura@f-plat.com>
2011-07-27 07:25:43 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cl::Application.config.secret_token = '2fef648a06cd0d2c46d471b995fd01722ace511fc620955b1e0862dbc901a3f1d36f6162c9914f672e23e04f50af56c1267350df69489b4c017cc95202713f8c'
